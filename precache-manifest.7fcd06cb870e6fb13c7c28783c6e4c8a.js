e2735e63865cf4dbb47d5a1eff7234ee356ad1e2
szal03 <33811775+szal03@users.noreply.github.com>
2020-08-19 13:22:06 +0200

    Updates

---
+++ precache-manifest.7fcd06cb870e6fb13c7c28783c6e4c8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cab5261d89ccc138558be32b98f8475",
+    "url": "/Planet-searcher/index.html"
+  },
+  {
+    "revision": "44d3ecce298feacee830",
+    "url": "/Planet-searcher/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "f7a500b16c49c0d038fa",
+    "url": "/Planet-searcher/static/css/main.99752be1.chunk.css"
+  },
+  {
+    "revision": "44d3ecce298feacee830",
+    "url": "/Planet-searcher/static/js/2.f88ed997.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Planet-searcher/static/js/2.f88ed997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7a500b16c49c0d038fa",
+    "url": "/Planet-searcher/static/js/main.13b52f04.chunk.js"
+  },
+  {
+    "revision": "8543e630677aeb6a947a",
+    "url": "/Planet-searcher/static/js/runtime-main.a7c1f05e.js"
+  },
+  {
+    "revision": "2b5803c31827971592dcac6060f32757",
+    "url": "/Planet-searcher/static/media/heroimg.2b5803c3.png"
+  }
+]);
